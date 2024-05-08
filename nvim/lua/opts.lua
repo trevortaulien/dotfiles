f9801fb3b43067fa0e5e7b57973f7c00c34360ca
trevor <51167085+trevortaulien@users.noreply.github.com>
2024-05-08 00:59:35 -0500

    Some inital neovim config files

---
+++ nvim/lua/opts.lua
@@ -0,0 +1,16 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.wrap = true
+vim.opt.breakindent = true
+
+vim.opt.clipboard = 'unnamedplus'
+
+-- For searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', lead = '·', trail = '·', nbsp = '␣'}
+
+vim.opt.hlsearch = true
