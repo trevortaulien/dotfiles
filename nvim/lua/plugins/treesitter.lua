506eecbfd5f45d00b0d80dd37cf1200ff72b5162
trevor <51167085+trevortaulien@users.noreply.github.com>
2024-05-12 02:53:06 -0500

    Attempted to add treesitter vhdl support. The parsing is working. The highlighting is not.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,12 +2,26 @@ return {
 	{
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
+		-- VHDL is not apart of treesitter repo so this is necessary
+		local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+		parser_config.vhdl = {
+		  install_info = {
+		    url = "https://github.com/alemuller/tree-sitter-vhdl", -- local path or git repo
+		    files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+		    -- optional entries:
+		    branch = "main", -- default branch in case of git repo if different from master
+		    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+		    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+		  },
+		  filetype = "vhdl", -- if filetype does not match the parser name
+		}
+		--
 		require("nvim-treesitter.configs").setup({
 		ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "python", "bash", "verilog",},
 		auto_install = false,
 		highlight = { enable = true, },
 		indent = true,
 		})
-		end
+	end
 	},
 }
